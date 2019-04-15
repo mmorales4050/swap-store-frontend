8dc13df7b7ed857600c43e16b42aacf069daf64e
mmorales4050 <mmorales4050@gmail.com>
2019-04-15 12:14:16 -0400

    added listing class

---
+++ src/listing.js
@@ -0,0 +1,21 @@
+class Listing {
+
+  static all = []
+
+  constructor(name, description, price, image, vendor_id, id) {
+    this.name = name
+    this.description = description
+    this.price = price
+    this.image = image
+    this.vendor_id = vendor_id
+    this.id = id
+  }
+
+  static fetch() {
+    
+  }
+
+  static rendorAll() {
+
+  }
+}
